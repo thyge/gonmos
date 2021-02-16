80273d25f2d4776044e124c3e1727e10d467d3b8
Thyge Haarberg <thygehaarberg@gmail.com>
2021-02-16 16:27:10 +0000

    temp update

---
+++ register/query.go
@@ -0,0 +1,9 @@
+package main
+
+type QuerySubscription struct {
+	MaxUpdateRateMs int           `json:"max_update_rate_ms"`
+	ResourcePath    string        `json:"resource_path"`
+	Params          []interface{} `json:"params"`
+	Persist         bool          `json:"persist"`
+	Secure          bool          `json:"secure"`
+}
